a00a2e0aef4c555e8e3186e1097d1a9ce32f9683
Amman Nega <ammannega@gmail.com>
2023-05-01 22:27:39 -0700

    prompts user to get data from all three tables

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,4 +2,14 @@ INSERT INTO department (name)
 VALUES  ("Engineering"),
         ("Human Resources"),
         ("Customer Service"),
-        ("Marketing")
+        ("Marketing");
+
+INSERT INTO role (title, salary, department_id)
+    VALUES  ("Software Engineer", 100000, 1),
+            ("HR Rep", 50000, 2),
+            ("Service Advisor", 20000, 3),
+            ("Sales Manager", 40000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+    VALUES  ("John", "Cena", 1, null),
+            ("Amman", "Nega", 1, 1);
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
 const db = mysql.createConnection(
@@ -10,10 +11,56 @@ const db = mysql.createConnection(
     console.log(`Connected to the companies_db database.`)  
 );
 
+db.connect();
+
+function trackEmployees() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "What would you like to do?",
+                choices: ["View All Departments", "Add A Department", "View All Roles", "Add A Role", "View All Employees", "Add An Employee", "Update An Employee Role", "Quit"],
+                name: "queryCompany",
+            }
+        ]).then(ans => {
+            if(ans.queryCompany === "View All Departments") {
+                getAllDepartments();
+            }
+            else if(ans.queryCompany === "View All Roles") {
+                getAllRoles();
+            }
+            else if(ans.queryCompany === "View All Employees") {
+                getAllEmployees();
+            }
+            else if(ans.queryCompany === "Quit") {
+                return; // exit program
+            }
+        })
+}
+
+// retrieves all rows in the department table
 function getAllDepartments() {
     db.query('SELECT * FROM department', function(err, results) {
-        console.log(results);
+        console.table(results);
+        trackEmployees(); // recursively call trackEmployees()
     });
 }
 
-getAllDepartments();
+// retrieves all rows in the roles table
+function getAllRoles() {
+    db.query('SELECT * FROM role', function(err, results) {
+        console.table(results);
+        trackEmployees(); // recursively call trackEmployees()
+    });
+}
+
+// retrieves all rows in the employees table
+function getAllEmployees() {
+    db.query('SELECT * FROM employee', function(err, results) {
+        console.table(results);
+        trackEmployees(); // recursively call trackEmployees()
+    });
+}
+
+// start program by prompting user
+trackEmployees();
